6b119dc0287e3437ab65086ef779f790d5a49e0f
BachelorDog <y.juntao@hotmail.com>
2019-08-03 11:30:24 +0800

    update

--- laminarFlow.py
+++ laminarFlow.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
+from fenics import *
+from dolfin import *
+from mshr import *
 import numpy as np
-import fenics as fe
 
 @dataclass
 class caseData:
@@ -9,24 +11,32 @@ class caseData:
     nx : int = 40
     ny : int = 30
 
+    inletLabel  : int = 1
+    outletLabel : int = 2
+    wallLabel   : int = 3
+
 def meshGeneration(caseData):
-    caseData.mesh = fe.RectangleMesh(fe.Point(0.0, -0.5*caseData.Ly), fe.Point(caseData.Lx, 0.5*caseData.Ly), caseData.nx, caseData.ny)
-    
-    caseData.inlet = fe.AutoSubDomain(lambda x, on_boundary: fe.near(x[0], 0.0) and on_boundary)
-    caseData.outlet = fe.AutoSubDomain(lambda x, on_boundary: fe.near(x[0], caseData.Ly) and on_boundary)
-    caseData.wall = fe.AutoSubDomain(lambda x, on_boundary: fe.near(abs(x[1]), 0.5*caseData.Ly) and on_boundary)
+    caseData.mesh = RectangleMesh(Point(0.0, -0.5*caseData.Ly), Point(caseData.Lx, 0.5*caseData.Ly), caseData.nx, caseData.ny)
 
-    caseData.boundaries = fe.MeshFunction("size_t", caseData.mesh, caseData.mesh.topology().dim()-1, 0)
-    caseData.ds = fe.ds(subdomain_data = caseData.boundaries)
+    caseData.inlet = AutoSubDomain(lambda x, on_boundary: near(x[0], 0.0) and on_boundary)
+    caseData.outlet = AutoSubDomain(lambda x, on_boundary: near(x[0], caseData.Ly) and on_boundary)
+    caseData.wall = AutoSubDomain(lambda x, on_boundary: near(abs(x[1]), 0.5*caseData.Ly) and on_boundary)
 
-    caseData.velocity = fe.VectorFunctionSpace(caseData.mesh, 'CG', 2)
-    caseData.pressure = fe.FunctionSpace(caseData.mesh, 'CG', 1)
+    caseData.boundaries = MeshFunction("size_t", caseData.mesh, caseData.mesh.topology().dim()-1, 0)
+    caseData.ds = ds(subdomain_data = caseData.boundaries)
 
-    v_elem = fe.VectorElement("CG", caseData.mesh.ufl_cell(), 1, 2)
-    p_elem = fe.FiniteElement("CG", caseData.mesh.ufl_cell(), 1)
-    caseData.MixedSpace = fe.FunctionSpace(caseData.mesh, fe.MixedElement([v_elem, p_elem]))
+    caseData.inlet.mark(caseData.boundaries, caseData.inletLabel)
+    caseData.outlet.mark(caseData.boundaries, caseData.outletLabel)
+    caseData.wall.mark(caseData.boundaries, caseData.wallLabel)
 
-    caseData.bc_wall = fe.DirichletBC(caseData.MixedSpace.sub(caseData.velocity), fe.Constant(0.0), caseData.boundaries)
+    caseData.velocity = VectorFunctionSpace(caseData.mesh, 'CG', 2)
+    caseData.pressure = FunctionSpace(caseData.mesh, 'CG', 1)
+
+    v_elem = VectorElement("CG", caseData.mesh.ufl_cell(), 1, 2)
+    p_elem = FiniteElement("CG", caseData.mesh.ufl_cell(), 1)
+    caseData.MixedSpace = FunctionSpace(caseData.mesh, MixedElement([v_elem, p_elem]))
+
+    caseData.bc_wall = DirichletBC(caseData.MixedSpace.sub(caseData.velocity).sub(1), Constant(0.0), caseData.boundaries, 3)
 
 if __name__=="__main__":
     laminarJet = caseData()
